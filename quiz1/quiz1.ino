4a6d8408e0269c88c6f909de453e79fd41161174
Crmayb2 <crmayb2@ilstu.edu>
2025-02-20 15:51:00 -0600

    Lights working

--- quiz1/quiz1.ino
+++ quiz1/quiz1.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int redPin = 9;
+const int greenPin = 10;
+const int bluePin = 11;
+
+const int redButton = 2;
+const int greenButton = 3;
+const int blueButton = 4;
+
+void setup() {
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+
+    pinMode(redButton, INPUT_PULLUP);
+    pinMode(greenButton, INPUT_PULLUP);
+    pinMode(blueButton, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int redPressed = digitalRead(redButton) == LOW;
+    int greenPressed = digitalRead(greenButton) == LOW;
+    int bluePressed = digitalRead(blueButton) == LOW;
 
+    analogWrite(redPin, redPressed ? 255 : 0);
+    analogWrite(greenPin, greenPressed ? 255 : 0);
+    analogWrite(bluePin, bluePressed ? 255 : 0);
 }
+
+
